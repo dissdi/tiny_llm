7ac176bb7e0ae3f45ec6ae16f345714e5ae40632
dissdi <iksdguri@gmail.com>
2026-03-11 07:57:48 +0000

    download dataset

---
+++ scripts/download_dataset.py
@@ -0,0 +1,19 @@
+from datasets import load_dataset, DatasetDict
+import os
+
+# Download and load the wikitext-2-raw-v1 dataset
+dataset = load_dataset('wikitext', 'wikitext-2-raw-v1')
+
+# Access the splits
+train_data = dataset['train']
+validation_data = dataset['validation']
+test_data = dataset['test']
+
+# Save dataset
+dataset_dict = DatasetDict({
+    "train": train_data,
+    "validation": validation_data,
+    "test": test_data,
+})
+
+dataset_dict.save_to_disk("datasets/wikitext-2-raw-v1")
---
+++ src/train.py
@@ -0,0 +1,22 @@
+import yaml
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# Load config
+def load_config(config_file_path):
+    with open(config_file_path, 'r') as file:
+        try:
+            config = yaml.safe_load(file)
+            return config
+        except:
+            print(f"Error reading yaml file")
+            return None
+
+config_data = load_config('configs/tiny_llm.yaml')
+
+# Model name
+model_name = "distilgpt2"
+
+# Load tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
